06687fb2061612d2742c83effe9c9baa65f0a9fe
Charpell <charpellumeh@gmail.com>
2018-05-22 04:26:35 +0100

    User Input: Mapping Input Control State

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -10,7 +10,30 @@ import backgroundImage from "../../assets/app.jpg"
 
 class AuthScreen extends Component {
     state = {
-        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape",
+        controls: {
+            email: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    isEmail: true
+                }
+            },
+            password: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    minLength: 6
+                }
+            },
+            confirmPassword: {
+                value: "",
+                valid: false,
+                validationRules: {
+                    equalTo: "password"
+                }
+            }
+        }
     }
     
     constructor(props) {
@@ -31,6 +54,21 @@ class AuthScreen extends Component {
     loginHandler = () => {
         startMainTabs()
     }
+
+    updateInputState = (key, value) => {
+        this.setState(prevState => {
+            return {
+                controls: {
+                    ...prevState.controls,
+                    [key] : {
+                        ...prevState.controls[key],
+                        value: value
+                    }
+                }
+            }
+        })
+    }
+
     render () {
         let headingText = null;
 
@@ -49,16 +87,31 @@ class AuthScreen extends Component {
                     <ButtonWithBackground color="#29aaf4" onPress={() => alert("Hello")}>Switch to Login</ButtonWithBackground>
 
                     <View style={styles.inputContainer}>
-                        <DefaultInput placeholder="Your email address" style={styles.input} />
+                        <DefaultInput 
+                            placeholder="Your email address" 
+                            style={styles.input} 
+                            value={this.state.controls.email.value} 
+                            onChangeText={(val) => this.updateInputState('email', val)}
+                        />
                         <View 
                             style={this.state.viewMode === "portrait" ? styles.portraitPasswordContainer : styles.landscapePasswordContainer}>
                             <View 
                                 style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                                <DefaultInput placeholder="Password" style={styles.input} />
+                                <DefaultInput 
+                                    placeholder="Password" 
+                                    style={styles.input}
+                                    value={this.state.controls.password.value} 
+                                    onChangeText={(val) => this.updateInputState('password', val)} 
+                                />
                             </View>
                             <View 
                                 style={this.state.viewMode === "portrait" ? styles.portraitPasswordWrapper : styles.landscapePasswordWrapper}>
-                                <DefaultInput placeholder="Confirm Password" style={styles.input} />
+                                <DefaultInput 
+                                    placeholder="Confirm Password" 
+                                    style={styles.input}
+                                    value={this.state.controls.confirmPassword.value} 
+                                    onChangeText={(val) => this.updateInputState('confirmPassword', val)} 
+                                />
                             </View>
                         </View>
                     </View>
